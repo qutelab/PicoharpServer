dc3bf2db6c8cd6bac6a7320b389f232b3910026c
Ben Graham <ben.n.graham@icloud.com>
2024-12-04 15:00:32 -0700

    First commit

---
+++ PicoharpServer.py
@@ -0,0 +1,95 @@
+import numpy as np
+from datetime import datetime
+import os
+from snAPI.Main import *
+import socket
+
+# Socket parameters
+HOST = '127.0.0.1'
+PORT = 65432
+
+# Setup Picoharp
+# select the device library
+sn = snAPI()
+# get first available device
+sn.getDevice()
+sn.setLogLevel(logLevel=LogLevel.DataFile, onOff=True)
+# initialize the device
+sn.initDevice(MeasMode.T2)
+# set the configuration for your device type
+sn.loadIniConfig("PicoharpConfig\HH.ini")
+
+
+
+def run_hist(identifer):
+    """
+
+    :param identifer: String ID for histogram to be saved with
+    :return: 0 for success, -1 for failure
+    """
+
+    sn.histogram.measure(acqTime=1000,savePTU=True)
+
+    data, bins = sn.histogram.getData()
+    data = data[0]
+
+    rst = np.zeros((len(data), 2))
+    rst[:, 0] = data
+    rst[:, 1] = bins
+    np.savetxt(f"output/{identifer}-hist-ph330.txt", rst)
+
+    return 0
+
+def get_histog_cps():
+    """
+
+    :return: Histogram cps
+    """
+
+    return np.random.randint(500000, 1000000)
+
+if __name__ == '__main__':
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            try:
+                conn, addr = s.accept()
+                with conn:
+                    #print('Connected by', addr)
+                    data = conn.recv(1024).decode()
+                    print(f"Received: {data}")
+                    split = data.split(" ")
+                    cmd = split[0]
+                    if len(split) == 2:
+                        param = data.split(" ")[1]
+                    else:
+                        param = ""
+                    print(f"Command: {cmd}, param: {param}")
+
+                    if cmd == 'run:hist':
+                        if param == "":
+                            print("No parameter provided for run:hist")
+                            rst = -1
+                        else:
+                            rst = run_hist(param)
+                        response = f'{cmd}={rst}'
+                        print(f"Sending response: {response}")
+                        conn.sendall(response.encode())
+                    elif cmd == 'get:hcps':
+                        response = f'{cmd}={get_histog_cps()}'
+                        print(f"Sending response: {response}")
+                        conn.sendall(response.encode())
+                    elif cmd == 'new:img':
+                        path = "output/scan" + str(datetime.now().strftime("%Y%m%d%H%M%S"))
+                        os.mkdir(path)
+                    else:
+                        print(f"Unknown command: {cmd}")
+                        response = f'{-1}'
+                        conn.sendall(response.encode())
+
+            except KeyboardInterrupt:
+                if conn:
+                    print("Closing connection")
+                    conn.close()
+                print("Closing server")
